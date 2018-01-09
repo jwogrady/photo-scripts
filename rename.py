75cb1de140c188b5d897478cb9024f477778074f
jwogrady <john@status26.com>
2018-01-09 09:27:17 -0600

    first commit

---
+++ rename.py
@@ -0,0 +1,31 @@
+import os
+import sys
+""" 
+Renames the filenames within the same directory to be Unix friendly
+(1) Changes spaces to hyphens
+(2) Makes lowercase (not a Unix requirement, just looks better ;)
+Usage:
+python rename.py
+"""
+
+dr = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+for subdir, dirs, files in os.walk(dr):
+    for filename in files:
+        subdirectoryPath = os.path.relpath(subdir, dr) #get the path to your subdirectory
+        filePath = os.path.join(subdirectoryPath, filename) #get the path to your file
+        print(filePath)
+        print(filename)
+
+        n = filePath.replace(' ', '-')
+        n = n.replace('/', "-")
+        n = n.replace('_', '-')
+        n = n.lower()
+
+        newFilePath = filePath.replace(filename, n) #create the new name
+
+        print(newFilePath)
+        print(n)
+
+        os.rename(filePath, newFilePath)
+        
